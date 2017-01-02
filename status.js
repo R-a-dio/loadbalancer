8c7f8b06ffdc3d1e89b3a20d2521cd82bdd10264
Amelia Ikeda <amelia@dorks.io>
2017-01-02 19:32:49 +0000

    Use es6 properly and update the status code

--- status.js
+++ status.js
@@ -1,108 +1,97 @@
 'use strict';
 
-let _ = require('lodash'),
-    request = require('request'),
-    relays = require('./relays.json'),
-    xml2js = require('xml2js').parseString,
-    args = require('minimist')(process.argv.slice(2)),
-    listeners = {},
-    fallback = "https://relay0.r-a-d.io/main.mp3",
-    chosen = fallback;
+const _ = require('lodash');
+const request = require('request');
+const fallback = process.env.FALLBACK_URL || "https://relay0.r-a-d.io/main.mp3";
+const xml = require('./xml');
+const display = require('./display');
 
-function logger(message) {
-  if (args.v) {
-    console.log(message)
-  }
-}
+let relays = require('./relays');
+let chosen = fallback;
 
-function check() {
-  _.forOwn(relays, (relay, key) =>
-    request(relay.links.status, (error, res, body) => {
-      if (error || ! body.length) {
-        logger("[" + key + "] transport error");
-        logger(error);
-        deactivate(key);
-        chosen = pick();
+const choose = () => chosen;
+const update = () => display.update(relays, chosen);
+
+const check = () => _.forOwn(relays, relay => {
+    if (relay.disabled) {
         return;
-      }
-
-      xml2js(body, (err, result) => {
-        let tracks = _.get(result, 'playlist.trackList');
-
-        if (tracks.length > 0) {
-          let count = parser(_.get(tracks, '0.track.0.annotation.0', ''));
-
-          relays[key].active = true;
-          relays[key].listeners = count;
-          listeners[key] = count;
-          chosen = pick();
-        } else {
-          deactivate(key);
-          chosen = pick();
-        }
-      })
-    })
-  )
-}
-
-function deactivate(key) {
-  logger("[" + key + "] deactivating");
-  relays[key].active = false;
-  relays[key].listeners = 0;
-  listeners[key] = 0;
-}
-
-function parser(annotation) {
-  var listeners = annotation.match(/Current Listeners: (\d+)/i);
-
-  return listeners != null ? parseInt(listeners[1], 10) : 0;
-}
-
-function choose() {
-  return chosen;
-}
-
-function pick() {
-  let min = null;
-  let relay = undefined;
-
-  for (var key in listeners) {
-    if (!relays[key].active || !relays[key].usable) continue;
-
-    let ratio = (listeners[key] / relays[key].max) - (relays[key].priority / 1000);
-
-    logger("[" + key + "] ratio: " + ratio);
-
-    if (!min || ratio < min) {
-      min = ratio;
-      relay = key;
     }
-  }
 
-  if (relay) {
-    logger("active relay is now: " + relays[relay].stream)
-    return relays[relay].stream;
-  }
+    request(relay.links.status, (error, res, body) => {
+        if (error || !body.length) {
+            return deactivate(relay);
+        }
 
-  logger("active relay is now [fallback]: " + fallback);
-  return fallback;
-}
+        xml.parse(body, count => activate(relay, count), () => deactivate(relay));
+    });
+});
 
-function status() {
-  let count = 0;
+const deactivate = relay => {
+    relay.online = false;
+    relay.listeners = 0;
 
-  for (var key in listeners) {
-    count += listeners[key];
-  }
+    chosen = pick();
+};
 
-  return {
-    relays: relays,
-    listeners: count
-  }
-}
+const activate = (relay, listeners) => {
+    relay.online = true;
+    relay.listeners = listeners;
+
+    chosen = pick();
+};
+
+const select = relay => {
+    _.forOwn(relays, function (relay) {
+        relay.primary = false;
+    });
+
+    if (relay) {
+        relay.primary = true;
+    }
+};
+
+const pick = () => {
+    let min = null;
+    let candidate = undefined;
+
+    _.forOwn(relays, relay => {
+        if (!relay.online || relay.disabled) {
+            return;
+        }
+
+        let ratio = (relay.listeners / relay.max) - (relay.priority / 1000);
+
+        if (!min || ratio < min) {
+            min = ratio;
+            candidate = relay;
+        }
+    });
+
+    if (candidate) {
+        select(candidate);
+
+        return candidate.links.stream;
+    }
+
+    select(false);
+
+    return fallback;
+};
+
+const status = () => {
+    let count = 0;
+
+    _.forOwn(relays, relay => count += relay.listeners);
+
+    return {
+        relays: relays,
+        total_listeners: count
+    }
+};
 
 module.exports = {
-  choose: choose,
-  relays: status,
-  check: check
-}
+    choose,
+    relays: status,
+    check,
+    update,
+};
